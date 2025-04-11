6b826817097f4f68b03db13f3c6c5792799488eb
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-04-11 19:12:24 -0300

    fix - remove flags and tags of the languages portuguese and english

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <header>
-      <span>
         <img className="logo" src="../src/assets/sun-cloud-logo.png" alt="Logo" />
         <h1>Previsão do Tempo</h1>
-      </span>
-      
-      <aside>
-        
-        <span>
-          <p>PT-BR</p>
-          <img className="flags" src="../src/assets/brazil.png" alt="Flag of the Brazil" />
-        </span>
-        
-        <span>
-          <p>EN</p>
-          <img className="flags" src="../src/assets/united-states.png" alt="Flag of the United States" width={40} />
-        </span>
-
-      </aside>
     </header>
   );
 }
